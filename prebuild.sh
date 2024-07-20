be8c8a7e79d642d9040110af508cd28707bdce85
jsjg-8 <jsjggroups@gmail.com>
2024-07-20 19:22:00 -0400

    feat: create prebuild.sh

---
+++ prebuild.sh
@@ -0,0 +1,10 @@
+﻿if ! asdf plugin-list | grep -q "bun"; then
+  asdf plugin add bun
+fi
+
+# Install the latest version of bun and set it globally
+asdf install bun latest
+asdf global bun latest
+
+# Install dependencies using bun
+bun i
