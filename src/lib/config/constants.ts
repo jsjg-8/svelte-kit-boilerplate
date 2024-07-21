a297d32d45683b172ab54a01c4211958a13e0afb
jsjg-8 <jsjggroups@gmail.com>
2024-07-20 21:36:44 -0400

    update constants.ts

--- src/lib/config/constants.ts
+++ src/lib/config/constants.ts
@@ -1,8 +1,8 @@
 import { dev } from '$app/environment';
-export const BASE_URL = dev ? 'http://localhost:5173' : 'https://sveltekit-auth-alpha.vercel.app';
+export const BASE_URL = dev ? 'http://localhost:5173' : 'https://svelte-kit-starter.pages.dev/';
 export const APP_NAME = 'Sveltekit Auth';
 export const CONTACT_EMAIL = 'yourname@email.com';
-export const DOMAIN = 'sveltekit-auth-alpha.vercel.app';
+export const DOMAIN = 'svelte-kit-starter.pages.dev';
 /* WARNING!!! TERMS AND CONDITIONS AND PRIVACY POLICY 
 WERE CREATED BY CHATGPT AS AN EXAMPLE ONLY. 
 CONSULT A LAWYER AND DEVELOP YOUR OWN TERMS AND PRIVACY POLICY!!! */
