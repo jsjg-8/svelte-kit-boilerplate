e95b5b1a90cf244aea1e1cdd3f2b2269855ebb29
jsjg-8 <jsjggroups@gmail.com>
2024-07-20 21:37:45 -0400

    update constants.ts

--- src/lib/config/constants.ts
+++ src/lib/config/constants.ts
@@ -1,16 +1,16 @@
 import { dev } from '$app/environment';
 export const BASE_URL = dev ? 'http://localhost:5173' : 'https://svelte-kit-starter.pages.dev/';
-export const APP_NAME = 'Sveltekit Auth';
-export const CONTACT_EMAIL = 'yourname@email.com';
+export const APP_NAME = 'Svelte-kit Starte';
+export const CONTACT_EMAIL = 'jsjggroups@gmail.com';
 export const DOMAIN = 'svelte-kit-starter.pages.dev';
 /* WARNING!!! TERMS AND CONDITIONS AND PRIVACY POLICY 
 WERE CREATED BY CHATGPT AS AN EXAMPLE ONLY. 
 CONSULT A LAWYER AND DEVELOP YOUR OWN TERMS AND PRIVACY POLICY!!! */
-export const TERMS_PRIVACY_CONTACT_EMAIL = 'yourname@email.com';
+export const TERMS_PRIVACY_CONTACT_EMAIL = 'jsjggroups@gmail.com';
 export const TERMS_PRIVACY_WEBSITE = 'yourdomain.com';
 export const TERMS_PRIVACY_COMPANY = 'Your Company';
-export const TERMS_PRIVACY_EFFECTIVE_DATE = 'January 1, 2023';
-export const TERMS_PRIVACY_APP_NAME = 'Your App';
+export const TERMS_PRIVACY_EFFECTIVE_DATE = 'January 7, 2024';
+export const TERMS_PRIVACY_APP_NAME = 'JSJG';
 export const TERMS_PRIVACY_APP_PRICING_AND_SUBSCRIPTIONS =
 	'[Details about the pricing, subscription model, refund policy]';
 export const TERMS_PRIVACY_COUNTRY = 'United States';
