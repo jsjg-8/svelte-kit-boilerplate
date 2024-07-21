a845083dea7d3f9b2dd91ed74341b8b5f53a67b5
jsjg-8 <jsjggroups@gmail.com>
2024-07-21 12:13:30 -0400

    update bun.lockb, package.json, +layout.server.ts and svelte.config.js

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,4 +2,3 @@ import { loadFlash } from 'sveltekit-flash-message/server';
 export const load = loadFlash(async (event) => {
 	return { user: event.locals.user };
 });
-export const prerender = true;
