9533ceb5e2dfa7df75beb8de2436a527ca07a875
jsjg-8 <jsjggroups@gmail.com>
2024-07-23 12:06:41 -0400

    add analytics and insights

---
+++ src/routes/+layout.ts
@@ -0,0 +1,8 @@
+﻿import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+
+inject({ mode: dev ? 'development' : 'production' });
+
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+injectSpeedInsights();
